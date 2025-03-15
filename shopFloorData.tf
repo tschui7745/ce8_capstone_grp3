bedc4306d46f343c9b0638948d185b83802d051c
chui <tschui7745@gmail.com>
2025-03-15 22:56:00 +0800

    1st commit

--- shopFloorData.tf
+++ shopFloorData.tf
@@ -304,7 +304,19 @@ resource "aws_api_gateway_stage" "stage-andon-api" {
     format          = "$context.requestId - $context.identity.sourceIp - $context.identity.userAgent - $context.requestTime - $context.status"
   }
 }
+
+# Create a KMS key for CloudWatch Logs encryption
+resource "aws_kms_key" "api_gateway_logs_kms" { # tschui added
+  description             = "KMS key for encrypting CloudWatch logs for API Gateway"
+  enable_key_rotation     = true
+}
+
+
 # CloudWatch Log Group for Access Logs
-resource "aws_cloudwatch_log_group" "api_gateway_logs" {
+resource "aws_cloudwatch_log_group" "api_gateway_logs" { # tschui added
   name = "/aws/api-gateway/shopFloorData-logs"
+  retention_in_days = 365  # Set retention period to 365 days
+
+   # Enabling encryption with customer-managed KMS key
+  kms_key_id = aws_kms_key.api_gateway_logs_kms.arn 
 }
